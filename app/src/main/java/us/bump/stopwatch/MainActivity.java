65912f43a0b9c1911ecb5f26dc6bc98a04afc074
Adam M. Bumpus <adam@bump.us>
2017-09-26 21:14:04 -0500

    I think that I have most things working correctly. Fixed the lap counter. Reset button works better. Added an additional display for time on current lap.

--- app/src/main/java/us/bump/stopwatch/MainActivity.java
+++ app/src/main/java/us/bump/stopwatch/MainActivity.java
@@ -1,20 +1,18 @@
 package us.bump.stopwatch;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.CompoundButton;
-import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Handler;
 
 import static android.os.SystemClock.elapsedRealtime;
 
@@ -23,15 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
     private int lapCounter;
     private long swElapsedTime;
-    private long lapStartTime;
+    private long swLapElapsedTime;
 
     private Button btnLap;
     private Button btnReset;
     private ToggleButton tbtnStartStop;
+    private LinearLayout ll;
 
     private Chronometer sw;
+    private Chronometer swLap;
     private TextView txtLap;
-    private TextView txtLapTimes;
+    private TextView currentLap;
 
     private Timer lockoutTimer;
 
@@ -49,9 +49,23 @@ public class MainActivity extends AppCompatActivity {
 
     private View.OnClickListener mbtnClickListener = new View.OnClickListener() {
         public void onClick(View v){
+            long currentTime = elapsedRealtime();
             switch (v.getId()) {
                 case R.id.btnLap:
                     btnLap.setEnabled(false);
+                    swLap.stop();
+
+                    TextView currentLap = new TextView(getApplicationContext());
+                    currentLap.setText(String.valueOf(lapCounter) + "     " + swLap.getText() + "     " + sw.getText());
+
+                    swLap.setBase(currentTime);
+                    swLapElapsedTime = 0;
+                    swLap.start();
+
+                    ll.addView(currentLap);
+
+                    txtLap.setText(String.valueOf(++lapCounter));
+
                     lockoutTimer.schedule(new TimerTask() {
                         @Override
                         public void run() {
@@ -63,15 +77,17 @@ public class MainActivity extends AppCompatActivity {
                             });
                         }
                     },500);
-                    txtLap.setText(String.valueOf(++lapCounter));
-                    txtLapTimes.setText(
-                            txtLapTimes.getText() +
-                            "\n"+ String.valueOf(lapCounter)+ " This Lap " + sw.getText());
+
                     break;
                 case R.id.btnReset:
                     txtLap.setText(String.valueOf(lapCounter=0));
                     swElapsedTime=0;
-                    sw.setBase(elapsedRealtime());
+                    sw.setBase(currentTime);
+                    swLapElapsedTime = 0;
+                    swLap.setBase(currentTime);
+                    ll.removeAllViewsInLayout();
+
+                    txtLap.setText(String.valueOf(lapCounter = 0));
                     break;
             }
         }
@@ -79,14 +95,24 @@ public class MainActivity extends AppCompatActivity {
 
     private CompoundButton.OnCheckedChangeListener mtglChangeListener = new CompoundButton.OnCheckedChangeListener(){
         public void onCheckedChanged(CompoundButton t, boolean isChecked) {
+            long currentTime = elapsedRealtime();
             if (isChecked) {
-                sw.setBase(elapsedRealtime()-swElapsedTime);
+                sw.setBase(currentTime - swElapsedTime);
                 sw.start();
+                swLap.setBase(currentTime - swElapsedTime);
+                swLap.start();
                 btnReset.setEnabled(false);
                 btnLap.setEnabled(true);
+
+                //If we're just starting, then set our lap # to 1
+                if (0 == lapCounter) {
+                    txtLap.setText(String.valueOf(++lapCounter));
+                }
             } else {
-                swElapsedTime= elapsedRealtime()-sw.getBase();
+                swElapsedTime = currentTime - sw.getBase();
                 sw.stop();
+                swLapElapsedTime = currentTime - swLap.getBase();
+                swLap.stop();
                 btnReset.setEnabled(true);
                 btnLap.setEnabled(false);
             }
@@ -104,8 +130,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Find the text areas in the content view
         sw = (Chronometer) findViewById(R.id.sw);
+        swLap = (Chronometer) findViewById(R.id.swLap);
         txtLap = (TextView) findViewById(R.id.txtLap);
-        txtLapTimes = (TextView) findViewById(R.id.txtLapTimes);
+        //ScrollView scrViewLapCounter = (ScrollView) findViewById(R.id.scrViewLapCounter);
+        ll = (LinearLayout) findViewById(R.id.linLayLapCounter);
+
 
         //Make Timer
         lockoutTimer = new Timer();
@@ -116,10 +145,13 @@ public class MainActivity extends AppCompatActivity {
         tbtnStartStop.setOnCheckedChangeListener(mtglChangeListener);
         tbtnStartStop.setOnClickListener(mbtnClickListener);
 
-
+        // Initialize the lapcounter and display
         lapCounter = 0;
-
         txtLap.setText(String.valueOf(lapCounter));
+
+        // Force the screen to stay on.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
 
     }
 
